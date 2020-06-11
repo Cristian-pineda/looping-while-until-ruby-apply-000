e45343ce191647608b956a71eeed1e2c5ec3a1e9
Cristian Pineda Leon <c.d.pineda96@gmail.com>
2020-06-11 16:25:00 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -3,7 +3,7 @@ def using_until
   Wingardium Leviosa = 6
   #your code here
   until Wingardium Leviosa == 10
-  puts "Wingardium Leviosa"
-  Wingardium Leviosa += 1
+    puts "Wingardium Leviosa"
+    Wingardium Leviosa += 1
  end
 end
